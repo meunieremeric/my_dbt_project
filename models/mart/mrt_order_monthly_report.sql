5f1564dc0acd5b9f114da62a7387f94df8cbf46a
e_meunier@live.fr <e_meunier@live.fr>
2025-07-05 14:41:21 +0000

    fix: update merge subquery

--- models/mart/mrt_order_monthly_report.sql
+++ models/mart/mrt_order_monthly_report.sql
@@ -5,8 +5,6 @@ SELECT DATE_TRUNC(order_date,month) AS order_month,
 COUNT(DISTINCT user_name) AS total_monthly_users
 FROM {{ source('sales_database', 'order')}}
 GROUP BY order_month
-ORDER BY total_monthly_users DESC
-
 
 ), total_monthly_user_from_jawa_timur AS (
 SELECT DATE_TRUNC(order_date,month) AS order_month,
@@ -20,7 +18,7 @@ GROUP BY order_month
 ), monthly_orders_recap AS (
 
 
-SELECT DATE_TRUNC(order_date,month) AS order_months,
+SELECT DATE_TRUNC(order_date,month) AS order_month,
 COUNT(order_id) AS total_monthly_orders
 FROM {{ source('sales_database', 'order')}}
 GROUP BY order_month
@@ -29,7 +27,7 @@ GROUP BY order_month
 )
 SELECT u.order_month,
 COALESCE(u.total_monthly_users,0) AS total_monthly_users,
-COALESCE(jt.total_monthly_users_from_jawa_timur,0) AS total_monthly_users_from_jawa_timur
+COALESCE(jt.total_monthly_users_from_jawa_timur,0) AS total_monthly_users_from_jawa_timur,
 COALESCE(o.total_monthly_orders,0) AS total_monthly_orders
 FROM monthly_users_recap AS u
 LEFT JOIN total_monthly_user_from_jawa_timur AS jt ON jt.order_month = u.order_month
