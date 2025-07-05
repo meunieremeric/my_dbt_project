f73728741b005c8409de4769a24c4893d5d77317
e_meunier@live.fr <e_meunier@live.fr>
2025-07-05 16:15:57 +0000

    feat:update

--- models/staging/sales_database/stg_sales_database__product.sql
+++ models/staging/sales_database/stg_sales_database__product.sql
@@ -1,8 +1,15 @@
 select
   product_id,
   product_category as product_category_name,
-  product_name_lenght,             
-  product_description_lenght,       
+  product_name_lenght,
+  product_description_lenght,
   product_photos_qty,
-  product_volume_cm3
+  product_weight_g,
+  product_length_cm,
+  product_height_cm,
+  product_width_cm,
+  
+  -- Calcul du volume en cm³ (longueur × largeur × hauteur)
+  product_length_cm * product_width_cm * product_height_cm as product_volume_cm3
+
 from {{ source('sales_database', 'product') }}
