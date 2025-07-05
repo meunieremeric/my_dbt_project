d46aa1b4e68d45b9bd7baf497a3a9c9e59c98076
meunieremeric <emeric.meunier@decathlon.com>
2025-07-05 15:32:04 +0200

    third commit

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -1,10 +1,10 @@
-select CONCAT(order_id, '_', product_id) AS order_item_id,
- order_id,
- product_id,
- 1 AS column_1,
- DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at,
- price as unit_price,
- shipping_cost,
- quantity as item_quantity,
- (price * quantity) + shipping_cost as total_order_item_amount
+ select CONCAT(order_id, '_', product_id) AS order_item_id_x,
+ order_id AS order_id_x,
+ product_id AS product_id_x,
+ 1 AS column_1_x,
+ DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_x,
+ price as unit_price_x,
+ shipping_cost AS shipping_cost_x,
+ quantity as item_quantity_x,
+ (price * quantity) + shipping_cost as total_order_item_amount_x
 from {{ source('sales_database', 'order_item') }}
